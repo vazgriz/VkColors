ea28c21c0d31e134be52ca1baa274a25cf7e1dd0
Unknown <vazgriz@gmail.com>
2018-05-29 14:31:03 -0500

    Add FPS counter

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
 #include "Core.h"
 
 int main() {
+    auto last = std::chrono::system_clock::now();
     glfwInit();
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -11,12 +15,25 @@ int main() {
 
     Core core = Core(window, 800, 600);
 
+    size_t frames = 0;
+
     glfwShowWindow(window);
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
         core.acquire();
         core.present();
+
+        frames++;
+        auto now = std::chrono::system_clock::now();
+        std::chrono::duration<float> elapsed = now - last;
+        if (elapsed.count() > 0.25f) {
+            std::stringstream builder;
+            builder << "Colors (" << std::setprecision(3) << static_cast<double>(frames) / elapsed.count() << " fps)";
+            glfwSetWindowTitle(window, builder.str().c_str());
+            frames = 0;
+            last = now;
+        }
     }
 
     glfwDestroyWindow(window);
