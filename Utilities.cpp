96ebb017e12332ce014546b9f37731be75eaaf5d
Unknown <vazgriz@gmail.com>
2018-05-29 17:21:37 -0500

    Add loadShader

--- Utilities.cpp
+++ Utilities.cpp
@@ -14,3 +14,12 @@ std::vector<char> loadFile(const std::string& path) {
 
     return result;
 }
+
+vk::ShaderModule loadShader(vk::Device& device, const std::string& path) {
+    std::vector<char> data = loadFile(path);
+
+    vk::ShaderModuleCreateInfo info = {};
+    info.code = std::move(data);
+    
+    return vk::ShaderModule(device, info);
+}
--- Utilities.h
+++ Utilities.h
@@ -1,3 +1,5 @@
 #include <vector>
+#include <VulkanWrapper/VulkanWrapper.h>
 
 std::vector<char> loadFile(const std::string& path);
+vk::ShaderModule loadShader(vk::Device& device, const std::string& path);
