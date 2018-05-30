dcafec5ef1441818d8bf18653c2ea7c04a01b9b6
Unknown <vazgriz@gmail.com>
2018-05-29 20:44:47 -0500

    Add size property

--- Bitmap.h
+++ Bitmap.h
@@ -20,6 +20,7 @@ public:
     size_t width() { return m_width; }
     size_t height() { return m_height; }
     void* data() { return m_data.data(); }
+    size_t size() { return m_width * m_height * 4; }
 
     Color32& getPixel(size_t x, size_t y) { return m_data[x + (y * m_width)]; }
 
